261bf26e29590716beb4191b88d649b117bdb53d
Angelo Moreno <emilioangelo665@gmail.com>
2023-10-09 15:44:37 +0200

    Esercizio 2 fatto

---
+++ SecondExercise.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SecondExercise {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        try {
+            System.out.println("Inserisci quanti km hai percorso");
+            int kmPercorsi = Integer.parseInt(input.nextLine());
+            if (kmPercorsi == 0) {
+                throw new ArithmeticException();
+            }
+            System.out.println("Inserisci quanti litri hai utilizzato");
+            int ltUsati = Integer.parseInt(input.nextLine());
+            double kmAlt = kmPercorsi / ltUsati;
+            System.out.println("Km/lt = " + kmAlt);
+        } catch (ArithmeticException ex) {
+            System.err.println("Non puoi dividere per 0!");
+        }
+        input.close();
+    }
+}
